86e82d721265e3bed2dee48f78aa20597bb92dfa
Luiz Roberto <passageiro1829@gmail.com>
2019-10-10 15:53:43 -0300

    adicionando funcao limpar

--- src/main/java/ifal/edu/br/formulario_js/Controlador.java
+++ src/main/java/ifal/edu/br/formulario_js/Controlador.java
@@ -1,8 +1,13 @@
 package ifal.edu.br.formulario_js;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controlador{
 
+    @RequestMapping("/")
+    public String index() {
+        return "ok";
+    }
 }
